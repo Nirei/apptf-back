624d2c49b1d69f622d7b1958096823f7a3ed1ccc
Nirro <nireiorange@gmx.com>
2018-05-23 00:34:43 +0200

    Adds /party/:creator/:date functionality.

--- src/endpoints/party/index.js
+++ src/endpoints/party/index.js
@@ -3,18 +3,27 @@ import * as partyDb from "../../data/party.js";
 
 const router = Router();
 
+router.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    next();
+});
+
 function listParties(req, res) {
   partyDb.listParties().then(parties => res.json(parties));
 }
 
-function getParty(req, res) {}
+function getParty(req, res) {
+  const creator = req.params.creator;
+  const date = parseInt(req.params.date);
+  partyDb.getParty({creator, date}).then(party => res.json(party));
+}
 
 function postParty(req, res) {}
 
 function deleteParty(req, res) {}
 
 router.get("/", listParties);
-router.get("/:partyCreator/:partyDate", getParty);
+router.get("/:creator/:date", getParty);
 router.post("/", postParty);
 router.delete("/", deleteParty);
 
