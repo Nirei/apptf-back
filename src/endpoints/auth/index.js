7bc294c95612ff2ff9f6d4ba0aaa85b42dc5fe31
Nirro <nireiorange@gmx.com>
2018-05-23 02:51:28 +0200

    Implements authentication for api endpoints.

--- src/endpoints/auth/index.js
+++ src/endpoints/auth/index.js
@@ -3,9 +3,31 @@ import passport from "passport";
 import LocalStrategy from "passport-local";
 import bcrypt from "bcryptjs";
 import * as authDb from "../../data/auth.js";
+import * as userDb from "../../data/user.js";
 
 const router = Router();
 const AUTH_FAIL = "Wrong username or password";
+
+function logout(req, res) {
+  req.logout();
+  res.redirect("/");
+}
+
+function changePassword(req, res) {
+  const id = req.params.id;
+
+}
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  userDb
+    .getUser({ id })
+    .then(user => done(null, user))
+    .catch(err => done(err));
+});
 
 passport.use(
   new LocalStrategy(function(username, password, done) {
@@ -14,7 +36,7 @@ passport.use(
       .then(auth => {
         if (!auth[0]) done(AUTH_FAIL);
         bcrypt.compare(password, auth[0].passwd, function(err, res) {
-          if(res) done(null, user);
+          if (res) done(null, user);
           else done(AUTH_FAIL);
         });
       })
@@ -22,6 +44,8 @@ passport.use(
   })
 );
 
+router.put("/password/:id", changePassword);
+router.get("/logout", logout);
 router.post(
   "/login",
   passport.authenticate("local", {
@@ -29,3 +53,5 @@ router.post(
     failureRedirect: "/login"
   })
 );
+
+export default router;
--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,17 +1,28 @@
 import express, { Router } from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+import passport from "passport";
+import config from "../../config.js";
 import party from "./party";
 import user from "./user";
+import auth from "./auth";
 
 const api = new Router();
 
-api.use(function (req, res, next) {
-    res.header('Content-Type', 'application/json');
-    next();
+api.use(session({ secret: config.API_SECRET, resave: false, saveUninitialized: false }));
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(passport.initialize());
+api.use(passport.session());
+api.use(function(req, res, next) {
+  res.header("Content-Type", "application/json");
+  next();
 });
 
+api.use("/auth", auth);
+api.use(passport.authenticate("local"));
 api.use("/user", user);
 api.use("/party", party);
 
-const statics = express.static('statics');
+const statics = express.static("statics");
 
 export default { api, statics };
