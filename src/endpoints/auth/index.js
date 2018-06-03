4cf77d4d07ebdc7b7776e6678ede4c5d0b4d3e86
Nirro <nireiorange@gmx.com>
2018-06-03 15:41:05 +0200

    Makes register/login work. Hashes passwords to store them in the DB.

--- src/endpoints/auth/index.js
+++ src/endpoints/auth/index.js
@@ -7,10 +7,50 @@ import * as userDb from "../../data/user.js";
 
 const router = Router();
 
+function login({ req, res, next }, user) {
+  req.logIn(user, function(err) {
+    if (err) {
+      return next(err); // 500 error
+    }
+    // Generate a JSON response reflecting signup
+    return res.json({ user });
+  });
+}
+
 function logout(req, res) {
-  console.log("logout");
   req.logout();
   res.redirect("/");
+}
+
+function register(req, res, next) {
+  const id = req.params.id;
+  Promise.all([userDb.getUser({ id }), authDb.getAuth({ id })])
+    .then(([[user], [auth]]) => {
+      if (auth) {
+        // User is already registered
+        res.status(409); // 409 Conflict
+        return res.end();
+      }
+      if (!user) {
+        // User id doesn't exist
+        res.status(403); // 403 Forbidden
+        return res.end();
+      }
+      // I call it plainText but should come hashed from client anyway.
+      const plainText = req.body.password;
+      if (typeof plainText !== "string") {
+        res.status(400);
+        return res.end();
+      }
+
+      bcrypt.hash(plainText, 10, function(err, password) {
+        authDb
+          .createAuth({ id, password })
+          .then(() => login({ req, res, next }, user))
+          .catch(err => next(err));
+      });
+    })
+    .catch(err => next(err));
 }
 
 function changePassword(req, res) {
@@ -18,38 +58,39 @@ function changePassword(req, res) {
 }
 
 passport.serializeUser(function(user, done) {
-  console.log(user);
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-  console.log(id);
   userDb
     .getUser({ id })
-    .then(user => done(null, user))
+    .then(([user]) => done(null, user))
     .catch(err => done(err));
 });
 
 passport.use(
-  new LocalStrategy(function(user, password, done) {
-    console.log("entering Local Strategy");
-    authDb
-      .getAuth({ id: user.id })
-      .then(auth => {
-        console.log(auth);
-        if (!auth[0]) {
-          console.log(auth);
-          console.log(user.id);
-          done(null, null);
-        } else {
-          bcrypt.compare(password, auth[0].passwd, function(err, res) {
-            if (res) {
-              done(null, user);
-            } else {
-              done(null, false);
-            }
-          });
+  new LocalStrategy(function(name, password, done) {
+    const failMessage = { message: "Incorrect username or password." };
+    userDb
+      .getUser({ name })
+      .then(([user]) => {
+        console.log(user);
+        if (!user) {
+          return done(null, false, failMessage);
         }
+        authDb.getAuth({ id: user.id }).then(([auth]) => {
+          if (!auth) {
+            done(null, false, failMessage);
+          } else {
+            bcrypt.compare(password, auth.password, function(err, res) {
+              if (res) {
+                done(null, user);
+              } else {
+                done(null, false, failMessage);
+              }
+            });
+          }
+        });
       })
       .catch(err => done(err));
   })
@@ -57,6 +98,7 @@ passport.use(
 
 router.put("/password/:id", changePassword);
 router.get("/logout", logout);
+router.post("/register/:id", register);
 router.post("/login", function(req, res, next) {
   passport.authenticate("local", function(err, user, info) {
     if (err) {
@@ -67,13 +109,7 @@ router.post("/login", function(req, res, next) {
       return res.end();
     }
     // We've got an user, attempt login
-    req.logIn(user, function(err) {
-      if (err) {
-        return next(err); // 500 error
-      }
-      // Generate a JSON response reflecting signup
-      return res.json({ success: true, user });
-    });
+    login({ req, res, next }, user);
   })(req, res, next);
 });
 
